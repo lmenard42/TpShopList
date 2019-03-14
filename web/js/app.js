0ae821d79d0bc77d0261ebc717276f85af018921
Exoleet <menard.ludovic@outlook.fr>
2019-03-14 15:26:42 +0100

    Add li whith checkbox and price and delete button

--- web/js/app.js
+++ web/js/app.js
@@ -1,6 +1,12 @@
 var ComponentList = {
     props: ['article'],
-    template: '<li class="list-group-item"> {{ article.text }} </li>'
+    template: '<li class="list-group-item"><center> <div class="row">'
+     + '<input type="text" v-model="article.id" hidden>'
+     + '<input class="ml-2" type="checkbox" v-model="article.checked">'
+     + '<input v-model="article.text" type="text" class="ml-2">' 
+     + '<input v-if="article.checked" size="5" class="ml-2" type="text" v-model="article.prix">'
+     + '<button class="btn btn-danger ml-2">X</button>'
+     + '</div></center></li>'
 }
 
 new Vue ({
@@ -13,13 +19,16 @@ new Vue ({
         emptyList: true,
         list:[],
         article: '',
-        id: 0
+        id: 0,
+        checked: false,
+        prix: 0
     },
     methods: {
         addArticle: function(){
-            this.list.push({id: this.id ,text: this.article})
+            this.list.push({id: this.id ,text: this.article, checked: this.checked, prix: this.prix})
             this.emptyList = false
             ++this.id
         }
+
     }
 })
